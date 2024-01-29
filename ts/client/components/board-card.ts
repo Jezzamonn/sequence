7f58a075c59cd80d145cd0bc59a0b953313786db
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2024-01-28 22:54:43 -0800

    Card images!

--- ts/client/components/board-card.ts
+++ ts/client/components/board-card.ts
@@ -1,6 +1,6 @@
 import { css, html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import { cardToShortString, Rank, Suit, suitToSymbol } from "../../common/cards";
+import { cardAssetName, cardToShortString, Rank, Suit, suitToSymbol } from "../../common/cards";
 
 @customElement("board-card")
 export class BoardCardElement extends LitElement {
@@ -9,13 +9,34 @@ export class BoardCardElement extends LitElement {
     // language=CSS
     css`
       :host {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background: #ffffff;
-        border: 1px solid black;
-        border-radius: 3px;
-        margin: 2px;
+        position: relative;
+
+        margin: 0.25cqw 0.25cqh;
+
+        max-width: 9.5cqw;
+        max-height: 9.5cqh;
+
+        background-color: white;
+        border-radius: 1cqw;
+
+        overflow: hidden;
+      }
+
+      .card-image {
+        max-width: 100%;
+        max-height: 100%;
+        object-fit: contain;
+        opacity: 0.5;
+      }
+
+      .card-label {
+        position: absolute;
+        background-color: white;
+        top: 0;
+        left: 0;
+        font-size: 20px;
+        text-align: center;
+        line-height: 1em;
       }
 
       .card-Spades {
@@ -26,16 +47,16 @@ export class BoardCardElement extends LitElement {
         color: red;
       }
 
-      .card-Diamonds {
-        color: red;
-      }
-
       .card-Clubs {
         color: black;
       }
 
+      .card-Diamonds {
+        color: red;
+      }
+
       .card-Joker {
-        color: #9bf;
+        display: none;
       }
       `
     ];
@@ -50,8 +71,21 @@ export class BoardCardElement extends LitElement {
     return cardToShortString({ rank: this.rank, suit: this.suit });
   }
 
+  get assetName() {
+    return cardAssetName({ rank: this.rank, suit: this.suit });
+  }
+
+  get ariaLabel() {
+    return `${this.shortString} of ${this.suit}`;
+  }
+
   render() {
     // language=HTML
-    return html`<div class="card card-${this.suit}">${this.shortString}</div>`;
+    // return html``;
+    // language=HTML
+    return html`
+      <img class="card-image" src="${this.assetName}">
+      <div class="card-label card-${this.suit}">${this.rank}<br>${suitToSymbol.get(this.suit)}</div>
+    `;
   }
 }
--- ts/common/cards.ts
+++ ts/common/cards.ts
@@ -9,6 +9,23 @@ export interface Card {
 export const allSuits: Suit[] = ["Spades", "Diamonds", "Clubs", "Hearts"];
 export const allRanks: Rank[] = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
+export const rankToFullName: Map<Rank, string> = new Map([
+    ["2", "2"],
+    ["3", "3"],
+    ["4", "4"],
+    ["5", "5"],
+    ["6", "6"],
+    ["7", "7"],
+    ["8", "8"],
+    ["9", "9"],
+    ["10", "10"],
+    ["J", "Jack"],
+    ["Q", "Queen"],
+    ["K", "King"],
+    ["A", "Ace"],
+    ["Joker", "Joker"],
+]);
+
 export const suitToSymbol: Map<Suit, string> = new Map([
     ["Clubs", "♣"],
     ["Diamonds", "♦"],
@@ -28,4 +45,17 @@ export function cardToShortString(card: Card): string {
         return "★";
     }
     return card.rank + suitToSymbol.get(card.suit);
+}
+
+export function cardAssetName(card: Card): string {
+    if (card.suit == "Joker") {
+        return "img/English_pattern_card_back.svg";
+    }
+    const fullRank = rankToFullName.get(card.rank)!.toLowerCase();
+    const suit = card.suit.toLowerCase();
+    return `img/English_pattern_${fullRank}_of_${suit}.svg`;
+}
+
+export function cardToLabel(card: Card): string {
+    return `${rankToFullName.get(card.rank)} of ${card.suit}`;
 }
